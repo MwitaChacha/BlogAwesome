e0a10b391e1a9e57bdae950722529d13d9aeaada
Mwita Chacha <jonahhchacha@gmail.com>
2021-11-14 10:22:01 +0300

    Adding user authentication configurations

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
 from wtforms.validators import InputRequired, Email, Length
 from wtforms import StringField, PasswordField, BooleanField
-from flask_wtf import FlaskForm
 from wtforms.widgets import TextArea
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+
 
 
 app = Flask(__name__)
@@ -13,6 +17,9 @@ app.config['SECRET_KEY'] = 'This_is_my_secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:6775@localhost/sunday'
 Bootstrap(app)
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 # MODELS
 class User(db.Model):
     __tablename__ = 'users'
