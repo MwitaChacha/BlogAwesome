c0a84607be9d7e944e58e2593d95b45614e7ccbf
Mwita Chacha <jonahhchacha@gmail.com>
2021-11-14 07:58:59 +0300

    Adding routes for respective templates

---
+++ run.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, redirect, url_for
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'This_is_my_secret'
+
+Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+@app.route('/failure')
+def failure():
+    return render_template('failure.html')
+
+@app.route('/dashboard')
+def index():
+    return render_template('dashboard.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
